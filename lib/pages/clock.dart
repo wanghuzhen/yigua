c57d9454a65a1c5ca3dacfe724312a7c026f2b08
wanghuzhen <2327253081@qq.com>
2021-07-25 21:26:51 +0800

    utils

--- lib/pages/clock.dart
+++ lib/pages/clock.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:amap_flutter_location/amap_flutter_location.dart';
 import 'package:amap_flutter_location/amap_location_option.dart';
 import 'package:permission_handler/permission_handler.dart';
+import '../utils/locationUtil.dart';
 
 class Clock extends StatefulWidget {
   const Clock({Key key}) : super(key: key);
@@ -19,23 +20,17 @@ class Clock extends StatefulWidget {
 }
 
 class _ClockState extends State<Clock> {
+  LocationUtil _lc = LocationUtil();
   Map<String, Object> _locationResult;
-
   StreamSubscription<Map<String, Object>> _locationListener;
-
   AMapFlutterLocation _locationPlugin = new AMapFlutterLocation();
 
   @override
   void initState() {
     super.initState();
-
-    /// 动态申请定位权限
+    // 动态申请定位权限
     requestPermission();
-
-    ///设置Android和iOS的apiKey<br>
-    AMapFlutterLocation.setApiKey("e0623716c6417f09425c2472d028317f", "");
-
-    ///注册定位结果监听
+    // 注册定位结果监听
     _locationListener = _locationPlugin
         .onLocationChanged()
         .listen((Map<String, Object> result) {
@@ -48,48 +43,41 @@ class _ClockState extends State<Clock> {
   @override
   void dispose() {
     super.dispose();
-
-    ///移除定位监听
+    //移除定位监听
     if (null != _locationListener) {
       _locationListener?.cancel();
     }
-
-    ///销毁定位
+    //销毁定位
     _locationPlugin.destroy();
   }
 
-  ///设置定位参数
+  //设置定位参数
   void _setLocationOption() {
     AMapLocationOption locationOption = new AMapLocationOption();
-
-    ///是否单次定位
+    //是否单次定位
     locationOption.onceLocation = true;
-
-    ///是否需要返回地理信息
+    //是否需要返回地理信息
     locationOption.needAddress = true;
-
-    ///逆地理信息的语言类型
+    //逆地理信息的语言类型
     locationOption.geoLanguage = GeoLanguage.DEFAULT;
     locationOption.fullAccuracyPurposeKey = "AMapLocationScene";
-
-    ///设置Android端的定位模式<br>
-    ///<li>[AMapLocationMode.Battery_Saving]</li>
-    ///<li>[AMapLocationMode.Device_Sensors]</li>
-    ///<li>[AMapLocationMode.Hight_Accuracy]</li>
+    //设置Android端的定位模式<br>
+    //<li>[AMapLocationMode.Battery_Saving]</li>
+    //<li>[AMapLocationMode.Device_Sensors]</li>
+    //<li>[AMapLocationMode.Hight_Accuracy]</li>
     locationOption.locationMode = AMapLocationMode.Hight_Accuracy;
-
-    ///将定位参数设置给定位插件
+    //将定位参数设置给定位插件
     _locationPlugin.setLocationOption(locationOption);
   }
 
-  ///开始定位
+  //开始定位
   void _startLocation() {
-    ///开始定位之前设置定位参数
+    //开始定位之前设置定位参数
     _setLocationOption();
     _locationPlugin.startLocation();
   }
 
-  ///停止定位
+  //停止定位
   void _stopLocation() {
     _locationPlugin.stopLocation();
   }
@@ -164,7 +152,7 @@ class _ClockState extends State<Clock> {
     ));
   }
 
-  /// 动态申请定位权限
+  //动态申请定位权限
   void requestPermission() async {
     // 申请权限
     bool hasLocationPermission = await requestLocationPermission();
@@ -175,8 +163,8 @@ class _ClockState extends State<Clock> {
     }
   }
 
-  /// 申请定位权限
-  /// 授予定位权限返回true， 否则返回false
+  // 申请定位权限
+  // 授予定位权限返回true， 否则返回false
   Future<bool> requestLocationPermission() async {
     //获取当前的权限
     var status = await Permission.location.status;
---
+++ lib/utils/locationUtil.dart
@@ -0,0 +1,77 @@
+/*
+ * @Author: Wang Huzhen
+ * @Version: 1.0
+ * @FilePath: \yigua\lib\utils\locationUtil.dart
+ * @Email: 2327253081@qq.com
+ * @Date: 2021-07-25 21:10:14
+ */
+import 'dart:async';
+import 'package:amap_flutter_location/amap_flutter_location.dart';
+import 'package:amap_flutter_location/amap_location_option.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class LocationUtil {
+  Map<String, Object> locationResult;
+  AMapFlutterLocation _locationPlugin = new AMapFlutterLocation();
+
+  //设置定位参数
+  void _setLocationOption() {
+    AMapLocationOption locationOption = new AMapLocationOption();
+    //是否单次定位
+    locationOption.onceLocation = true;
+    //是否需要返回地理信息
+    locationOption.needAddress = true;
+    //逆地理信息的语言类型
+    locationOption.geoLanguage = GeoLanguage.DEFAULT;
+    locationOption.fullAccuracyPurposeKey = "AMapLocationScene";
+    //设置Android端的定位模式<br>
+    //<li>[AMapLocationMode.Battery_Saving]</li>
+    //<li>[AMapLocationMode.Device_Sensors]</li>
+    //<li>[AMapLocationMode.Hight_Accuracy]</li>
+    locationOption.locationMode = AMapLocationMode.Hight_Accuracy;
+    //将定位参数设置给定位插件
+    _locationPlugin.setLocationOption(locationOption);
+  }
+
+  //开始定位
+  void startLocation() {
+    //开始定位之前设置定位参数
+    _setLocationOption();
+    _locationPlugin.startLocation();
+  }
+
+  //停止定位
+  void stopLocation() {
+    _locationPlugin.stopLocation();
+  }
+
+  //动态申请定位权限
+  void requestPermission() async {
+    // 申请权限
+    bool hasLocationPermission = await requestLocationPermission();
+    if (hasLocationPermission) {
+      print("定位权限申请通过");
+    } else {
+      print("定位权限申请不通过");
+    }
+  }
+
+  // 申请定位权限
+  // 授予定位权限返回true， 否则返回false
+  Future<bool> requestLocationPermission() async {
+    //获取当前的权限
+    var status = await Permission.location.status;
+    if (status == PermissionStatus.granted) {
+      //已经授权
+      return true;
+    } else {
+      //未授权则发起一次申请
+      status = await Permission.location.request();
+      if (status == PermissionStatus.granted) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
